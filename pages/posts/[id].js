66761c84154bf8f53ee23fd284b829e1cae6b9f4
egurinko <egctoru@gmail.com>
2021-01-01 23:18:53 +0900

    create post dynamic route page file

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Head from "next/head"
+import Layout from "../../components/layout"
+import { getAllPostIds, getPostData } from "../../lib/posts"
+
+const Post = ({ postData }) => {
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </Layout>
+    )
+}
+
+export default Post
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }){
+    const postData = await getPostData(params.id)
+
+    return {
+        props: {
+            postData
+        }
+    }
+}
